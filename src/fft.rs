d61e9f49d0a1ed37e9b0bed8ef02f69b751df916
Hugo Åkerlund <hugo@hugoakerlund.net>
2026-09-06 16:16:00 +0300

    Generate nth roots of unity

--- src/fft.rs
+++ src/fft.rs
@@ -1,4 +1,3 @@
-use std::f64::consts::E;
 use std::f64::consts::PI;
 use num::complex::Complex;
 
@@ -10,6 +9,18 @@ pub fn get_even_elements(arr: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
 pub fn get_odd_elements(arr: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
     let result: Vec<Complex<f64>> = arr.iter().step_by(2).copied().collect();
     result
+}
+
+pub fn get_nth_roots_of_unity(n: usize) -> Vec<Complex<f64>> {
+    let mut result: Vec<Complex<f64>> = std::vec::from_elem(Complex::new(0.0, 0.0), n);
+    for k in 0 .. n {
+        let angle: f64 = (2.0 * PI * k as f64) / n as f64;
+        let real: f64 = (angle.cos() * 1000000.0).round() / 1000000.0;
+        let img: f64 = (angle.sin() * 1000000.0).round() / 1000000.0;
+        result[k] = Complex::new(real, img);
+    }
+    result
+
 }
 
 pub fn fft(mut arr: Vec<Complex<f64>>) -> Vec<Complex<f64>> {
--- tests/test_fft.rs
+++ tests/test_fft.rs
@@ -56,4 +56,19 @@ mod tests {
         assert_eq!(result, expected);
     }
 
+    #[test]
+    fn nth_roots_of_unity_are_generated_correctly() -> () {
+        let n: usize = 3;
+
+        let result: Vec<Complex<f64>> = fft::get_nth_roots_of_unity(n);
+
+        let expected: Vec<Complex<f64>> = vec![
+            Complex::new(1.0, 0.0),
+            Complex::new(-0.5, 0.866025),
+            Complex::new(-0.5, -0.866025),
+        ];
+
+        assert_eq!(result, expected);
+
+    }
 }
